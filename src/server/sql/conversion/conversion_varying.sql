5a5744d8bc3b4b38c83fa54a250b8381bc1c59f0
niccm <104477989+niccm@users.noreply.github.com>
2024-04-05 12:55:29 -0700

    Table

---
+++ src/server/sql/conversion/conversion_varying.sql
@@ -0,0 +1,12 @@
+ CREATE TABLE IF NOT EXISTS conversions_time(
+        source_id INTEGER NOT NULL REFERENCES units(id),
+        destination_id INTEGER NOT NULL REFERENCES units(id),
+        bidirectional BOOLEAN NOT NULL,
+        start_time TIMESTAMP, --  TIMESTAMP ‘-infinity’
+        end_time TIMESTAMP, --  TIMESTAMP ‘infinity’
+        slope FLOAT, --this is the rate
+        intercept FLOAT,
+        note TEXT,
+        CHECK (source_id != destination_id),
+        PRIMARY KEY (source_id, destination_id)
+    );
